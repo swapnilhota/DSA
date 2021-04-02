6a4893a488e854dd7ddaee5d6256fe94e0327833
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-02 14:24:34 +0530

    Update

--- LinkedLists/InsertingNode/insertingNode.cpp
+++ LinkedLists/InsertingNode/insertingNode.cpp
@@ -10,7 +10,7 @@ public:
 };
 
 //insert a node in the front of the list
-void addFront(Node** head_ref, int new_data)
+void push(Node** head_ref, int new_data)
 {
     Node* new_node = new Node();
 
@@ -21,6 +21,7 @@ void addFront(Node** head_ref, int new_data)
     (*head_ref) = new_node;
 }
 
+//insert a node after the given node
 void insertAfter(Node* prev_node, int new_data)
 {
 
@@ -39,7 +40,70 @@ void insertAfter(Node* prev_node, int new_data)
     prev_node->next = new_node;
 }
 
+//insert node at the end of the list
+void append(Node** head_ref, int new_data)
+{
+    Node* new_node = new Node();
+
+    new_node->data = new_data;
+
+    new_node->next = NULL;
+
+    Node* last = *head_ref;
+
+    if(*head_ref == NULL)
+    {
+        *head_ref = new_node;
+        return;
+    }
+
+    while(last->next != NULL)
+    {
+        last = last->next;
+    }
+
+    last->next = new_node;
+
+    return;
+}
+
+void printList(Node* node)
+{
+    while(node!=NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+
+    cout << endl;
+}
+
 int main()
 {
+    /* Start with the empty list */
+    Node* head = NULL;
+
+    // Insert 6. So linked list becomes 6->NULL
+    append(&head, 6);
+
+    // Insert 7 at the beginning.
+    // So linked list becomes 7->6->NULL
+    push(&head, 7);
+
+    // Insert 1 at the beginning.
+    // So linked list becomes 1->7->6->NULL
+    push(&head, 1);
+
+    // Insert 4 at the end. So
+    // linked list becomes 1->7->6->4->NULL
+    append(&head, 4);
+
+    // Insert 8, after 7. So linked
+    // list becomes 1->7->8->6->4->NULL
+    insertAfter(head->next, 8);
+
+    cout<<"Created Linked list is: ";
+    printList(head);
+
     return 0;
 }
