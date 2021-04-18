239fb5ad8dfbc57f2ce33467147ac51a9e6e60a7
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-18 20:31:23 +0530

    remove duplicate

---
+++ LinkedLists/RemoveDuplicateSortedLL/code.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+	Node* next;
+	int data;
+	Node(int val) {
+		next=NULL;
+		data=val;
+	}
+};
+
+void push(Node** headRef, int newData) {
+	Node* newNode = new Node(newData);
+	if(*headRef==NULL) {
+		*headRef=newNode;
+		return;
+	}
+	newNode->next=*headRef;
+	*headRef=newNode;
+	return;
+}
+
+void printList(Node* head) {
+	while(head!=NULL) {
+		cout << head->data << " ";
+		head=head->next;
+	}
+	cout << endl;
+	return;
+}
+
+int main() {
+
+	Node* head=NULL;
+	push(&head, 1);
+	push(&head, 1);
+	push(&head, 2);
+	push(&head, 2);
+	push(&head, 3);
+	push(&head, 4);
+
+	printList(head);
+
+	return 0;
+}
