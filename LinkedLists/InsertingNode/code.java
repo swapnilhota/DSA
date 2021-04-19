03e03915b4f889ef91121b2878bc47df1f52ac14
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-19 19:23:29 +0530

    Java file added

--- LinkedLists/InsertingNode/code.java
+++ LinkedLists/InsertingNode/code.java
@@ -1,66 +1,73 @@
-public class code {
+class LinkedList {
 
-	class Node {
-		int data;
-		Node next;
 
-		Node(int val) {
-			data=val;
-			next=null;
+		Node head;
+
+		class Node {
+
+			int data;
+			Node next;
+
+			Node(int val) {
+				data=val;
+				next=null;
+			}
 		}
-	}
 
-	public void push(Node head, int newData) {
-		Node newNode = new Node(newData);
-		newNode.next=head;
-		head=newNode;
-	}
+		public void push(int newData) {
+			Node newNode = new Node(newData);
+			newNode.next=head;
+			head=newNode;
+		}
 
-	public void insertAfter(Node prevNode, int newData) {
-		if(prevNode==null) {
+		public void insertAfter(Node prevNode, int newData) {
+			if(prevNode==null) {
+				return;
+			}
+
+			Node newNode = new Node(newData);
+			newNode.next = prevNode.next;
+			prevNode.next = newNode;
+		}
+
+		public void append(int newData) {
+			Node newNode = new Node(newData);
+
+			if(head==null) {
+				head = newNode;
+				return;
+			}
+
+			Node last = head;
+			while(last.next!=null) {
+				last=last.next;
+			}
+			last.next=newNode;
 			return;
 		}
 
-		Node newNode = new Node(newData);
-		newNode.next = prevNode.next;
-		prevNode.next = newNode;
-	}
+		public void printList() {
+			Node temp = head;
+			while(temp!=null) {
+				System.out.print(temp.data+" ");
+				temp=temp.next;
+			}
+		}	
 
-	public void append(Node head, int newData) {
-		Node newNode = new Node(newData);
-
-		if(head==null) {
-			head = newNode;
-			return;
-		}
-
-		Node last = head;
-		while(last.next!=null) {
-			last=last.next;
-		}
-		last.next=newNode;
-		return;
-	}
-
-	public void printList(Node head) {
-		Node temp = head;
-		while(temp!=null) {
-			System.out.print(temp.data+" ");
-			temp=temp.next;
-		}
-	}
+	
 
 	public static void main(String[] args) {
 		
-		Node head = null;
+		LinkedList list = new LinkedList();
 
-		push(head, 1);
-		push(head, 2);
-		push(head, 3);
-		push(head, 4);
-		push(head, 5);
+		list.append(1);
+		list.append(2);
+		list.append(3);
+		list.append(4);
+		list.append(5);
 
-		printList(head);
+
+		list.printList();
 
 	}
 }
