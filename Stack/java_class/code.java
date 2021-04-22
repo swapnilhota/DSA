a5c24d3d9a496806a84b2d46aa22bccf6bc96233
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-22 20:21:39 +0530

    stack java class

---
+++ Stack/java_class/code.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class code {
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+	}
+}
