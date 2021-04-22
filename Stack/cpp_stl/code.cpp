2df6cd276f91c50149fad99d73bd251da8c89912
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-22 20:15:16 +0530

    stack cpp_stl

--- Stack/cpp_stl/code.cpp
+++ Stack/cpp_stl/code.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main() {
 
-	
-	
+	stack<int> stk;
+	stk.push(1);
+	stk.push(2);
+	stk.push(3);
+	stk.push(4);
+
+	while(!stk.empty()) {
+		cout << stk.top() << " ";
+		stk.pop();
+	}
+	cout << endl;
+
+	stk.pop();
+
+	while(!stk.empty()) {
+		cout << stk.top() << " ";
+		stk.pop();
+	}
+
 	return 0;
 }
