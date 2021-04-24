c16feb808f4f274163e1ba3f2052f0cdb83f2ba9
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-24 21:40:46 +0530

    Special Stack

--- Stack/SpecialStack/cpp/code.cpp
+++ Stack/SpecialStack/cpp/code.cpp
@@ -9,7 +9,7 @@ private:
 public:
 	void push(int x) {
 		s.push(x);
-		if(s_min.Empty()) {
+		if(s_min.empty()) {
 			s_min.push(x);
 		}
 		else {
@@ -20,38 +20,47 @@ public:
 	}
 
 	int top() {
+		if(s.empty()) {
+			exit(0);
+		}
 		return s.top();
 	}
 
 	void pop() {
+		if(s.empty()) {
+			exit(0);
+		}
 		s.pop();
 	}
 
 	bool isEmpty() {
-		if(s.Empty()) {
+		if(s.empty()) {
 			return true;
 		}
 		return false;
 	}
 
 	int min() {
-		if(s_min.Empty()) {
+		if(s_min.empty()) {
 			return -1;
 		}
 		else {
 			return s_min.top();
 		}
 	}
-}
+};
 
 int main() {
 
-	SpecialStack spl_stack;
+	SpecialStack s;
 
-	push(spl_stack, 1);
-	push(spl_stack, 2);
-	push(spl_stack, 3);
-	push(spl_stack, 4);
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+
+	cout << s.min() << endl;
+	cout << s.top() << endl;
 
 	return 0;
 }
