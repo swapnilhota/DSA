0227108289be1798a86435470434f6a48a25d247
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-26 16:08:09 +0530

    Priority Queue

---
+++ Queue/priority_queue/cpp/code.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void showpq(priority_queue<int> gq) {
+	priority_queue<int> g = gq;
+	while(!g.empty()) {
+		cout << '\t' << g.top();
+		g.pop();
+	}
+	cout << endl;
+}
+
+int main() {
+
+	priority_queue<int> gquiz;
+
+	gquiz.push(1);
+	gquiz.push(2);
+	gquiz.push(3);
+	gquiz.push(4);
+
+	cout << "The priority queue gquiz is : ";
+    showpq(gquiz);
+  
+    cout << "\ngquiz.size() : " << gquiz.size();
+    cout << "\ngquiz.top() : " << gquiz.top();
+  
+    cout << "\ngquiz.pop() : ";
+    gquiz.pop();
+    showpq(gquiz);
+
+	return 0;
+}
