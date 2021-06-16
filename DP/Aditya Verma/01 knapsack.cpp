2591c0280824b6ac33ceb4100fb25499a0f4cd6a
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-06-16 12:12:58 +0530

    Update 01 knapsack.cpp

--- DP/Aditya Verma/01 knapsack.cpp
+++ DP/Aditya Verma/01 knapsack.cpp
@@ -10,9 +10,11 @@ n -> array size
 
 */
 
+/************************ normal recursive solution ***********************/
+
 int knapsack(int wt[], int val[], int W, int n)
 {
-  //normal recursive solution
+  
   
   //Base Case->
   if(n==0 || W==0)
@@ -29,3 +31,37 @@ int knapsack(int wt[], int val[], int W, int n)
     return knapsack(wt, val, W, n-1);
   }
 }
+
+/********************** memoization ************************/
+
+// make dp array for variables which are changing in recursive calls, like in this question, size of array (n) and capacity of knapsack (W)
+// declaring a global array according to the constraints of the question i.e. max values of variables which are changing in recursive calls 
+// initialize the dp array with -1 using memset() in main function.
+int dp[102][1002];
+
+int knapsack(int wt[], int val[], int W, int n)
+{
+  //Base Case->
+  if(n==0 || W==0)
+  {
+    return 0;
+  }
+  
+  //check dp array
+  if(dp[n][W]!=-1)
+  {
+    return dp[n][W];
+  }
+  
+  if(wt[n-1]<=W)
+  {
+    // store value before returning
+    return dp[n][W] = max(val[n-1]+knapsack(wt, val, W-wt[n-1], n-1), knapsack(wt, val, W, n-1));        
+  }
+  else
+  {
+    // store value before returning
+    return dp[n][W] = knapsack(wt, val, W, n-1);
+  }
+}
+            
