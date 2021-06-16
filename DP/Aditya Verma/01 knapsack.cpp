6dcc5bbadda8db6ce6d9ec719b4751d9edf280c2
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-06-16 12:47:06 +0530

    Update 01 knapsack.cpp

--- DP/Aditya Verma/01 knapsack.cpp
+++ DP/Aditya Verma/01 knapsack.cpp
@@ -70,3 +70,44 @@ int knapsack(int wt[], int val[], int W, int n)
 // need to make same array as in memoization
 // step1 = initialize the array
 // step2 = change recursive calls to interative calls
+// base condition in recursive solution is used for initialization
+
+int knapsack(int wt[], int val[], int W, int n)
+{
+  // dp array to store values for subproblems
+  int dp[n+1][W+1]; 
+  
+  // initialize the dp array
+  for(int i=0; i<=n; i++)
+  {
+    for(int j=0; j<=W; j++)
+    {
+      if(i==0 || j==0)
+      {
+        dp[i][j]=0;
+      }
+    }
+  }
+  
+  // solving iteratively
+  // i -> n
+  // j -> W
+  
+  for(int i=1; i<=n; i++)
+  {
+    for(int j=1; j<=W; j++)
+    {
+      if(wt[i-1] <= j)
+      {
+        dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]], dp[i-1][j]);
+      }
+      else 
+      {
+        dp[i][j] = dp[i-1][j];
+      }
+    }
+  }
+  
+  return dp[n][W];
+  
+}
