12bb58dec9202b86149dabe729fccbe6d148dea5
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-07-09 17:50:10 +0530

    Update Equal Sum Partition.cpp

--- DP/Aditya Verma/Equal Sum Partition.cpp
+++ DP/Aditya Verma/Equal Sum Partition.cpp
@@ -3,3 +3,28 @@ Given an array, determine whether we can partition the array into two subsets wi
 return true or false
 */
 
+int main()
+{
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i=0; i<n; i++) cin>>a[i];
+
+	int sum=0;
+	for(auto i: arr)
+	{
+		sum+=i;
+	}
+
+	if(sum%2!=0)
+	{
+		cout<<0<<endl;
+		return 0;
+	}
+
+	solve(arr, n, sum);
+
+	return 0;
+
+}
