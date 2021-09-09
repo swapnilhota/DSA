3247be881530b10542d3a8310ca015181b06ccdc
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-09-09 19:04:15 +0530

    Create Print Shortest Common Supersequence.cpp

---
+++ DP/Aditya Verma/Print Shortest Common Supersequence.cpp
@@ -0,0 +1,103 @@
+/*
+Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences. If there are multiple valid strings, return any of them.
+
+A string s is a subsequence of string t if deleting some number of characters from t (possibly 0) results in the string s.
+
+ 
+
+Example 1:
+
+Input: str1 = "abac", str2 = "cab"
+Output: "cabac"
+Explanation: 
+str1 = "abac" is a subsequence of "cabac" because we can delete the first "c".
+str2 = "cab" is a subsequence of "cabac" because we can delete the last "ac".
+The answer provided is the shortest such string that satisfies these properties.
+Example 2:
+
+Input: str1 = "aaaaaaaa", str2 = "aaaaaaaa"
+Output: "aaaaaaaa"
+ 
+
+Constraints:
+
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of lowercase English letters.
+
+*/
+
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m=str1.length();
+        int n=str2.length();
+        int dp[m+1][n+1];
+        for(int i=0; i<=m; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(str1[i-1]==str2[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        string scs="";
+        
+        int i=m, j=n;
+        
+        while(i>0 && j>0)
+        {
+            if(str1[i-1]==str2[j-1])
+            {
+                scs += str1[i-1];
+                i--;
+                j--;
+            }
+            else
+            {
+                if(dp[i-1][j]>dp[i][j-1])
+                {
+                    scs += str1[i-1];
+                    i--;
+                }
+                else
+                {
+                    scs += str2[j-1];
+                    j--;
+                }
+            }
+        }
+        
+        while(i>0)
+        {
+            scs += str1[i-1];
+            i--;
+        }
+        while(j>0)
+        {
+            scs += str2[j-1];
+            j--;
+        }
+        
+        reverse(scs.begin(), scs.end());
+        
+        return scs;
+    }
+};
